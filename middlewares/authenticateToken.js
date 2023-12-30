4dbfa5accad949f7cac1bb24599ed16b46fc2898
kunal <kunaljoshi0603@gmail.com>
2023-12-31 00:51:20 +0530

    token authentication done

---
+++ middlewares/authenticateToken.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+
+exports.authenticateToken = (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token)
+    return res.status(401).json({ message: "Access denied. Token missing." });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: "Invalid token." });
+    req.user = user;
+    next();
+  });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const { authenticateToken } = require("../middlewares/authenticateToken");
 
 //Login
 router.post("/login", async (req, res) => {
@@ -26,10 +27,29 @@ router.post("/login", async (req, res) => {
     }
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
+      expiresIn: "10h",
     });
 
     res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//Profile
+router.get("/profile", authenticateToken, async (req, res) => {
+  try {
+    const userId = req.user.userId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    const { password, ...userWithoutPassword } = user.toObject();
+
+    res.status(200).json({ user: userWithoutPassword });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
